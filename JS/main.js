f02b72999252bb4c334d01453d92e95ce3e60747
Jamie <james.cranston91@gmail.com>
2015-05-13 23:00:52 +1000

    Fixed nav bar hide animation

--- JS/main.js
+++ JS/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         fullScreenButtn = $('.fullScreen'),
         sliderCont = $('.slider'),
         indic = $('.indicators'),
-        navBar = $('.navbar-fixed');
+        navBar = $('nav');
         indic.css('bottom', '');
     $(".dropdown-button").dropdown();
      $(".button-collapse").sideNav();
@@ -14,30 +14,34 @@ $(document).ready(function(){
         if(isFullScreen){
             sliderCont.css('height','');
             $('.welcomeArea').css("marginTop", "");
-         fullScreenButtn.css('top', '340px');
-            indic.animate({top:'340px'}, 1000, function(){
+         fullScreenButtn.css('top', '330px');
+         sliderCont.toggleClass('fullscreen');
+         isFullScreen = false;
+         navBar.show(0).animate({top:"0px"},1000);
+            indic.animate({top:'340px'}, 1000);
              sliderCont.animate({height:"400px"}, 1000);
-            });
-            sliderCont.toggleClass('fullscreen');
-        isFullScreen = false;
-        navBar.show().animate({top:"-100px"},1000);
-            $('html, body').css({
-    'overflow': 'auto',
-    'height': 'auto'
-});
-        }else{
+               $('html, body').css({
+       'overflow': 'auto',
+       'height': 'auto'
+           });
+
+
+
+}else{
            sliderCont.css('height','');
             indic.css('top','');
           sliderCont.toggleClass('fullscreen');
         isFullScreen = true;
-        navBar.animate({top:"0"},1000).hide();
+        navBar.animate({top:"-100px"},1000).delay(1050).hide(0);
             $('html, body').css({
     'overflow': 'hidden',
     'height': '100%'
                 });
             fullScreenButtn.css('top', '75%');
             $('.welcomeArea').animate({marginTop: '50%'}, 1050);
-            indic.animate({top:'750px'},1000);
+              indic.animate({top:'750px'},1000);
+
+
         }
 
     });

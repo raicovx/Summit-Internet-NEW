503580b694dfa747b526cc6c0b46f2edcbd2f37b
Jamie <james.cranston91@gmail.com>
2015-06-03 00:49:49 +1000

    Social icons added and IE fixes

--- index.php
+++ index.php
@@ -12,24 +12,29 @@
     <nav>
         <div class="nav-wrapper">
         <a href="#!" class="brand-logo"><img src="Images/logotransp.png"></a>
-       
-         
+
+
              <?php include('navigation-dropdown-internet.php');
             include('navigation-dropdown-hosting.php');
-            include('navigation-dropdown-members.php'); 
-            include('navigation-main.php'); 
-            include('navigation-slideOutMenu.php');?>  
-            
+            include('navigation-dropdown-members.php');
+            include('navigation-main.php');
+            include('navigation-slideOutMenu.php');?>
+
      </div>
     </nav>
-    </div>  
-     
+    </div>
+
     <div class="row">
-              <?php include('content-slider.php'); ?>   
+              <?php include('content-slider.php'); ?>
     </div>
       <main>
-               <div class="container"> 
-    <?php 
+               <div class="container">
+                 <div class="socialIcons">
+                    <a href="https://www.facebook.com/pages/Summit-Internet/164742417010031?fref=ts" target="_blank"><img src="Images/facebook.png"/></a>
+                    <a href="https://plus.google.com/110786601121239409350/posts" target="_blank"><img src="Images/google+.png"/></a>
+                    <a href="http://www.linkedin.com/company/3531677?trk=tyah&trkInfo=clickedVertical%3Acompany%2Cidx%3A1-1-1%2CtarId%3A1433255968137%2Ctas%3Asummit%20internet" target="_blank"><img src="Images/linkedin.png"/></a>
+                 </div>
+    <?php
     include('content-welcome-area.php');
     include('content-internet-section.php');?>
 <div class="adverts center card col s12 m12 l10 offset-l1">
@@ -65,7 +70,7 @@
     </div>
     </div>
         </main>
-   
+
 <script src="JS/materialize.min.js"></script>
     <script src="JS/main.js"></script>
 </body>
